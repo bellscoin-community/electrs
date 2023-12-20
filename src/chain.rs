651fe6795bf4d43a5c2dfd161703c071728b84d7
BlobMaster41 <96896824+BlobMaster41@users.noreply.github.com>
2023-12-20 02:51:39 -0500

    Update chain.rs

--- src/chain.rs
+++ src/chain.rs
@@ -67,7 +67,7 @@ impl Chain {
     }
 
     pub(self) fn hash_u8(hash: &str) -> Vec<u8> {
-        let data = match <[u8; 64]>::from_hex(hash) {
+        let data = match <[u8; 32]>::from_hex(hash) {
             Ok(res) => res.to_vec(), // Convert array to Vec
             Err(err) => panic!("{}", err)
         };
